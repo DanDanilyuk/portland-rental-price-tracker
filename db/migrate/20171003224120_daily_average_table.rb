d6e8e175572781e79b4360138565b767e5f2a2f7
Nate <30516063+hal2814@users.noreply.github.com>
2017-10-03 16:01:57 -0700

    Update and rename 20171003224120_daily_average_tabel.rb to 20171003224120_daily_average_table.rb

--- db/migrate/20171003224120_daily_average_table.rb
+++ db/migrate/20171003224120_daily_average_table.rb
@@ -2,11 +2,11 @@ class DailyAverageTabel < ActiveRecord::Migration[5.1]
   def change
     create_table(:averages) do |t|
 
-      t.column(:avg_1br, :string)
+      t.column(:avg_1br, :int)
       t.column(:avg_2br, :int)
       t.column(:avg_3br, :int)
       t.column(:avg_4br, :int)
-      t.column(:section, :sting)
+      t.column(:section, :string)
 
       t.timestamps
     end
